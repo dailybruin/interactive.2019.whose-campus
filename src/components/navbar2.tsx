61a0dd734b61385eb1b732628ef27fc66758ad70
Richard <richbluehill@gmail.com>
2019-04-04 13:55:21 -0700

    nav bar changes

--- src/components/navbar2.tsx
+++ src/components/navbar2.tsx
@@ -1,118 +1,57 @@
 import * as React from 'react'
-import { css } from 'react-emotion'
-import Navbar from 'react-bootstrap/Navbar'
-import Button from 'react-bootstrap/Button'
-import FormControl from 'react-bootstrap/FormControl'
-import Form from 'react-bootstrap/Form'
-import Nav from 'react-bootstrap/Nav'
+import styled, { css } from 'react-emotion'
 
 class NavBar2 extends React.Component {
   render() {
+    type navLinkProps = {
+      backColor: string
+      width: string
+      children: string
+      href: string
+    }
+    const NavLink = styled.a`
+      padding: 8px 15px;
+      text-decoration: none;
+      font-family: Montserrat;
+      text-transform: lowercase;
+      font-weight: ${(props: navLinkProps) =>
+        props.width === '50%' ? 'bold' : 'normal'};
+      color: #ffffff;
+      font-size: ${(props: navLinkProps) =>
+        props.width === '50%' ? '2.25rem' : '1.5rem'};
+      width: ${(props: navLinkProps) => props.width};
+      background: ${(props: navLinkProps) => props.backColor};
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `
     return (
       <div>
-        <ul
+        <div
           className={css`
             width: 100%;
-            float: left;
             margin: 0 0 3em 0;
             padding: 0;
             list-style: none;
             display: flex;
-            flex-direction: row;
-            flex-wrap: nowrap;
-            height: 100%;
+            height: 75px;
+            @media screen and (max-height: 700px) {
+            }
           `}
         >
-          <li
-            className={css`
-              float: left;
-            `}
-          >
-            <a
-              href="#"
-              className={css`
-                display: block;
-                padding: 8px 15px;
-                text-decoration: none;
-                font-family: Montserrat;
-                text-transform: lowercase;
-                font-weight: bold;
-                color: #ffffff;
-
-                left: 0px;
-                top: 0px;
-                flex-grow: 1;
-
-                background: #161a3e;
-              `}
-            >
-              Whose Campus
-            </a>
-          </li>
-          <li
-            className={css`
-              float: left;
-            `}
-          >
-            <a
-              href="#"
-              className={css`
-                display: block;
-                padding: 8px 15px;
-                text-decoration: none;
-                font-weight: bold;
-                font-family: Montserrat;
-                text-transform: lowercase;
-                color: #ffffff;
-                background: #6966a3;
-              `}
-            >
-              Equity
-            </a>
-          </li>
-          <li
-            className={css`
-              float: left;
-            `}
-          >
-            <a
-              href="#"
-              className={css`
-                display: block;
-                padding: 8px 15px;
-                text-decoration: none;
-                font-weight: bold;
-                font-family: Montserrat;
-                text-transform: lowercase;
-                color: #ffffff;
-                background: #56449d;
-              `}
-            >
-              Diversity
-            </a>
-          </li>
-          <li
-            className={css`
-              float: left;
-            `}
-          >
-            <a
-              href="#"
-              className={css`
-                display: block;
-                padding: 8px 15px;
-                text-decoration: none;
-                font-weight: bold;
-                font-family: Montserrat;
-                text-transform: lowercase;
-                color: #ffffff;
-                background: #46457d;
-              `}
-            >
-              Inclusion
-            </a>
-          </li>
-        </ul>
+          <NavLink href="#" width="50%" backColor="#161A3E">
+            Whose Campus
+          </NavLink>
+          <NavLink href="#" width="16.667%" backColor="#6966a3">
+            Equity
+          </NavLink>
+          <NavLink href="#" width="16.667%" backColor="#56449d">
+            Diversity
+          </NavLink>
+          <NavLink href="#" width="16.667%" backColor="#46457d">
+            Inclusion
+          </NavLink>
+        </div>
       </div>
     )
   }
