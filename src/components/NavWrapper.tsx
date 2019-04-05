2888d8bf21d0fbc7fa3ffcd264763a027ba2fcde
Richard <richbluehill@gmail.com>
2019-04-04 21:06:58 -0700

    still idk

--- src/components/NavWrapper.tsx
+++ src/components/NavWrapper.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { css } from 'react-emotion'
-import NavBar2 from './Navbar2'
+import NavBar2 from './navbar2'
 import { DropdownMenu } from './DropdownMenu'
 
 interface NavWrapperProps {
